b2bf138c163fb60459d5dff00c1b5f33f1db5381
Gary Tokman <yo@garytokman.com>
2025-04-16 15:13:20 -0400

    fix: remove plugin

--- package/plugin/withIosDeploymentTarget.js
+++ package/plugin/withIosDeploymentTarget.js
@@ -109,111 +109,111 @@ fi`;
   );
 
   // reference the "props"
-  const { version, repositoryUrl, repoName, productName } = {
-    repositoryUrl: "https://github.com/candlefinance/candle-swift",
-    repoName: "candle-swift",
-    productName: "Candle",
-    version: "fa0db96e9a73740bbd4977160894a45c6db96f51",
-  };
-  // get XCRemoteSwiftPackageReference section
-  const spmReferences =
-    xcodeProject.hash.project.objects["XCRemoteSwiftPackageReference"];
-  // if doesn't exist (this is our first SPM package) create empty object
-  if (!spmReferences) {
-    xcodeProject.hash.project.objects["XCRemoteSwiftPackageReference"] = {};
-  }
-  // generate new ID
-  const packageReferenceUUID = xcodeProject.generateUuid();
-  // add XCRemoteSwiftPackageReference section
-  xcodeProject.hash.project.objects["XCRemoteSwiftPackageReference"][
-    `${packageReferenceUUID} /* XCRemoteSwiftPackageReference "${repoName}" */`
-  ] = {
-    isa: "XCRemoteSwiftPackageReference",
-    repositoryURL: repositoryUrl,
-    requirement: {
-      kind: "revision",
-      revision: version,
-    },
-  };
+  // const { version, repositoryUrl, repoName, productName } = {
+  //   repositoryUrl: "https://github.com/candlefinance/candle-swift",
+  //   repoName: "candle-swift",
+  //   productName: "Candle",
+  //   version: "fa0db96e9a73740bbd4977160894a45c6db96f51",
+  // };
+  // // get XCRemoteSwiftPackageReference section
+  // const spmReferences =
+  //   xcodeProject.hash.project.objects["XCRemoteSwiftPackageReference"];
+  // // if doesn't exist (this is our first SPM package) create empty object
+  // if (!spmReferences) {
+  //   xcodeProject.hash.project.objects["XCRemoteSwiftPackageReference"] = {};
+  // }
+  // // generate new ID
+  // const packageReferenceUUID = xcodeProject.generateUuid();
+  // // add XCRemoteSwiftPackageReference section
+  // xcodeProject.hash.project.objects["XCRemoteSwiftPackageReference"][
+  //   `${packageReferenceUUID} /* XCRemoteSwiftPackageReference "${repoName}" */`
+  // ] = {
+  //   isa: "XCRemoteSwiftPackageReference",
+  //   repositoryURL: repositoryUrl,
+  //   requirement: {
+  //     kind: "revision",
+  //     revision: version,
+  //   },
+  // };
 
   // get XCSwiftPackageProductDependency section
-  const spmProducts =
-    xcodeProject.hash.project.objects["XCSwiftPackageProductDependency"];
-  // if doesn't exist (this is our first SPM package) create empty object
-  if (!spmProducts) {
-    xcodeProject.hash.project.objects["XCSwiftPackageProductDependency"] = {};
-  }
-  // generate new ID
-  const packageUUID = xcodeProject.generateUuid();
-  // add XCSwiftPackageProductDependency section
-  xcodeProject.hash.project.objects["XCSwiftPackageProductDependency"][
-    `${packageUUID} /* ${productName} */`
-  ] = {
-    isa: "XCSwiftPackageProductDependency",
-    // from step before
-    package: `${packageReferenceUUID} /* XCRemoteSwiftPackageReference "${repoName}" */`,
-    productName: productName,
-  };
+  // const spmProducts =
+  //   xcodeProject.hash.project.objects["XCSwiftPackageProductDependency"];
+  // // if doesn't exist (this is our first SPM package) create empty object
+  // if (!spmProducts) {
+  //   xcodeProject.hash.project.objects["XCSwiftPackageProductDependency"] = {};
+  // }
+  // // generate new ID
+  // const packageUUID = xcodeProject.generateUuid();
+  // // add XCSwiftPackageProductDependency section
+  // xcodeProject.hash.project.objects["XCSwiftPackageProductDependency"][
+  //   `${packageUUID} /* ${productName} */`
+  // ] = {
+  //   isa: "XCSwiftPackageProductDependency",
+  //   // from step before
+  //   package: `${packageReferenceUUID} /* XCRemoteSwiftPackageReference "${repoName}" */`,
+  //   productName: productName,
+  // };
 
   // get main project ID
-  const projectId = Object.keys(
-    xcodeProject.hash.project.objects["PBXProject"]
-  ).at(0);
-  // create empty array for package references if it doesn't exist
-  if (
-    !xcodeProject.hash.project.objects["PBXProject"][projectId][
-      "packageReferences"
-    ]
-  ) {
-    xcodeProject.hash.project.objects["PBXProject"][projectId][
-      "packageReferences"
-    ] = [];
-  }
-  // add our package reference (use ID from first step)
-  xcodeProject.hash.project.objects["PBXProject"][projectId][
-    "packageReferences"
-  ] = [
-    ...xcodeProject.hash.project.objects["PBXProject"][projectId][
-      "packageReferences"
-    ],
-    `${packageReferenceUUID} /* XCRemoteSwiftPackageReference "${repoName}" */`,
-  ];
+  // const projectId = Object.keys(
+  //   xcodeProject.hash.project.objects["PBXProject"]
+  // ).at(0);
+  // // create empty array for package references if it doesn't exist
+  // if (
+  //   !xcodeProject.hash.project.objects["PBXProject"][projectId][
+  //     "packageReferences"
+  //   ]
+  // ) {
+  //   xcodeProject.hash.project.objects["PBXProject"][projectId][
+  //     "packageReferences"
+  //   ] = [];
+  // }
+  // // add our package reference (use ID from first step)
+  // xcodeProject.hash.project.objects["PBXProject"][projectId][
+  //   "packageReferences"
+  // ] = [
+  //   ...xcodeProject.hash.project.objects["PBXProject"][projectId][
+  //     "packageReferences"
+  //   ],
+  //   `${packageReferenceUUID} /* XCRemoteSwiftPackageReference "${repoName}" */`,
+  // ];
 
-  // generate new ID
-  const frameworkUUID = xcodeProject.generateUuid();
-  // add comment and reference to our framework in PBXBuildFile section
-  xcodeProject.hash.project.objects["PBXBuildFile"][
-    `${frameworkUUID}_comment`
-  ] = `${productName} in Frameworks`;
-  xcodeProject.hash.project.objects["PBXBuildFile"][frameworkUUID] = {
-    isa: "PBXBuildFile",
-    productRef: packageUUID,
-    productRef_comment: productName,
-  };
+  // // generate new ID
+  // const frameworkUUID = xcodeProject.generateUuid();
+  // // add comment and reference to our framework in PBXBuildFile section
+  // xcodeProject.hash.project.objects["PBXBuildFile"][
+  //   `${frameworkUUID}_comment`
+  // ] = `${productName} in Frameworks`;
+  // xcodeProject.hash.project.objects["PBXBuildFile"][frameworkUUID] = {
+  //   isa: "PBXBuildFile",
+  //   productRef: packageUUID,
+  //   productRef_comment: productName,
+  // };
 
-  // get first build phase
-  const buildPhaseId = Object.keys(
-    xcodeProject.hash.project.objects["PBXFrameworksBuildPhase"]
-  ).at(0);
-  // create empty array for files if it doesn't exist
-  if (
-    !xcodeProject.hash.project.objects["PBXFrameworksBuildPhase"][buildPhaseId][
-      "files"
-    ]
-  ) {
-    xcodeProject.hash.project.objects["PBXFrameworksBuildPhase"][buildPhaseId][
-      "files"
-    ] = [];
-  }
-  // add our framework reference (use ID from step 4)
-  xcodeProject.hash.project.objects["PBXFrameworksBuildPhase"][buildPhaseId][
-    "files"
-  ] = [
-    ...xcodeProject.hash.project.objects["PBXFrameworksBuildPhase"][
-      buildPhaseId
-    ]["files"],
-    `${frameworkUUID} /* ${productName} in Frameworks */`,
-  ];
+  // // get first build phase
+  // const buildPhaseId = Object.keys(
+  //   xcodeProject.hash.project.objects["PBXFrameworksBuildPhase"]
+  // ).at(0);
+  // // create empty array for files if it doesn't exist
+  // if (
+  //   !xcodeProject.hash.project.objects["PBXFrameworksBuildPhase"][buildPhaseId][
+  //     "files"
+  //   ]
+  // ) {
+  //   xcodeProject.hash.project.objects["PBXFrameworksBuildPhase"][buildPhaseId][
+  //     "files"
+  //   ] = [];
+  // }
+  // // add our framework reference (use ID from step 4)
+  // xcodeProject.hash.project.objects["PBXFrameworksBuildPhase"][buildPhaseId][
+  //   "files"
+  // ] = [
+  //   ...xcodeProject.hash.project.objects["PBXFrameworksBuildPhase"][
+  //     buildPhaseId
+  //   ]["files"],
+  //   `${frameworkUUID} /* ${productName} in Frameworks */`,
+  // ];
 
   return config;
 };
