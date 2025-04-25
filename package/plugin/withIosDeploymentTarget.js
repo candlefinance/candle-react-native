491f0cae9b091e7cba35de19a0a03c076dca7bb0
Gary Tokman <yo@garytokman.com>
2025-04-24 21:42:58 -0400

    fix: update plugin

--- package/plugin/withIosDeploymentTarget.js
+++ package/plugin/withIosDeploymentTarget.js
@@ -20,12 +20,6 @@ const plugin = (config, options = {}) => {
         },
       ],
     ],
-    [
-      withXcodeProject,
-      async (config) => {
-        return withMyCustomBuildPhase(config);
-      },
-    ],
   ]);
 };
 
@@ -56,18 +50,9 @@ const withIosDeploymentTarget = async (config) => {
   }
 
   const setPreInstallHook = mergeContents({
-    tag: "rnreanimated-preinstall",
+    tag: "",
     src: setDeploymentTarget.contents,
-    newSrc: `  pre_install do |installer|
-    installer.pod_targets.each do |pod|
-      if pod.name.eql?('RNReanimated')
-        def pod.build_type;
-          # This is a workaround for the issue with the dynamic library
-          Pod::BuildType.static_library;
-        end
-      end
-    end
-  end`,
+    newSrc: ``,
     anchor: /post_install do \|installer\|/i,
     offset: 0,
     comment: "#",
@@ -79,141 +64,6 @@ const withIosDeploymentTarget = async (config) => {
   }
 
   fs.writeFileSync(podfile, setPreInstallHook.contents);
-
-  return config;
-};
-
-// This function adds a custom build phase to the Xcode project
-// that removes signature files from the build directory
-// for Xcode 15 and 16. This is a workaround for a known issue
-// with these versions of Xcode.
-// I think it's because of the duplicate symbol issue.
-const withMyCustomBuildPhase = async (config) => {
-  // Duplicate symbols issue
-  const xcodeProject = config.modResults;
-  const shellScript = `if { [ "$XCODE_VERSION_MAJOR" = "1500" ] || [ "$XCODE_VERSION_MAJOR" = "1600" ]; } && [ "$CONFIGURATION" = "Release" ]; then
-  echo "Remove signature files (Xcode 15/16 workaround, only in Release)"
-  find "$BUILD_DIR/\${CONFIGURATION}-iphoneos" -name "*.signature" -type f | xargs -r rm
-fi`;
-
-  xcodeProject.addBuildPhase(
-    [],
-    "PBXShellScriptBuildPhase",
-    "Remove Framework signature files (Xcode 15/16 workaround)",
-    null,
-    {
-      shellPath: "/bin/sh",
-      shellScript,
-      runOnlyForDeploymentPostprocessing: 1,
-    }
-  );
-
-  // reference the "props"
-  // const { version, repositoryUrl, repoName, productName } = {
-  //   repositoryUrl: "https://github.com/candlefinance/candle-swift",
-  //   repoName: "candle-swift",
-  //   productName: "Candle",
-  //   version: "fa0db96e9a73740bbd4977160894a45c6db96f51",
-  // };
-  // // get XCRemoteSwiftPackageReference section
-  // const spmReferences =
-  //   xcodeProject.hash.project.objects["XCRemoteSwiftPackageReference"];
-  // // if doesn't exist (this is our first SPM package) create empty object
-  // if (!spmReferences) {
-  //   xcodeProject.hash.project.objects["XCRemoteSwiftPackageReference"] = {};
-  // }
-  // // generate new ID
-  // const packageReferenceUUID = xcodeProject.generateUuid();
-  // // add XCRemoteSwiftPackageReference section
-  // xcodeProject.hash.project.objects["XCRemoteSwiftPackageReference"][
-  //   `${packageReferenceUUID} /* XCRemoteSwiftPackageReference "${repoName}" */`
-  // ] = {
-  //   isa: "XCRemoteSwiftPackageReference",
-  //   repositoryURL: repositoryUrl,
-  //   requirement: {
-  //     kind: "revision",
-  //     revision: version,
-  //   },
-  // };
-
-  // get XCSwiftPackageProductDependency section
-  // const spmProducts =
-  //   xcodeProject.hash.project.objects["XCSwiftPackageProductDependency"];
-  // // if doesn't exist (this is our first SPM package) create empty object
-  // if (!spmProducts) {
-  //   xcodeProject.hash.project.objects["XCSwiftPackageProductDependency"] = {};
-  // }
-  // // generate new ID
-  // const packageUUID = xcodeProject.generateUuid();
-  // // add XCSwiftPackageProductDependency section
-  // xcodeProject.hash.project.objects["XCSwiftPackageProductDependency"][
-  //   `${packageUUID} /* ${productName} */`
-  // ] = {
-  //   isa: "XCSwiftPackageProductDependency",
-  //   // from step before
-  //   package: `${packageReferenceUUID} /* XCRemoteSwiftPackageReference "${repoName}" */`,
-  //   productName: productName,
-  // };
-
-  // get main project ID
-  // const projectId = Object.keys(
-  //   xcodeProject.hash.project.objects["PBXProject"]
-  // ).at(0);
-  // // create empty array for package references if it doesn't exist
-  // if (
-  //   !xcodeProject.hash.project.objects["PBXProject"][projectId][
-  //     "packageReferences"
-  //   ]
-  // ) {
-  //   xcodeProject.hash.project.objects["PBXProject"][projectId][
-  //     "packageReferences"
-  //   ] = [];
-  // }
-  // // add our package reference (use ID from first step)
-  // xcodeProject.hash.project.objects["PBXProject"][projectId][
-  //   "packageReferences"
-  // ] = [
-  //   ...xcodeProject.hash.project.objects["PBXProject"][projectId][
-  //     "packageReferences"
-  //   ],
-  //   `${packageReferenceUUID} /* XCRemoteSwiftPackageReference "${repoName}" */`,
-  // ];
-
-  // // generate new ID
-  // const frameworkUUID = xcodeProject.generateUuid();
-  // // add comment and reference to our framework in PBXBuildFile section
-  // xcodeProject.hash.project.objects["PBXBuildFile"][
-  //   `${frameworkUUID}_comment`
-  // ] = `${productName} in Frameworks`;
-  // xcodeProject.hash.project.objects["PBXBuildFile"][frameworkUUID] = {
-  //   isa: "PBXBuildFile",
-  //   productRef: packageUUID,
-  //   productRef_comment: productName,
-  // };
-
-  // // get first build phase
-  // const buildPhaseId = Object.keys(
-  //   xcodeProject.hash.project.objects["PBXFrameworksBuildPhase"]
-  // ).at(0);
-  // // create empty array for files if it doesn't exist
-  // if (
-  //   !xcodeProject.hash.project.objects["PBXFrameworksBuildPhase"][buildPhaseId][
-  //     "files"
-  //   ]
-  // ) {
-  //   xcodeProject.hash.project.objects["PBXFrameworksBuildPhase"][buildPhaseId][
-  //     "files"
-  //   ] = [];
-  // }
-  // // add our framework reference (use ID from step 4)
-  // xcodeProject.hash.project.objects["PBXFrameworksBuildPhase"][buildPhaseId][
-  //   "files"
-  // ] = [
-  //   ...xcodeProject.hash.project.objects["PBXFrameworksBuildPhase"][
-  //     buildPhaseId
-  //   ]["files"],
-  //   `${frameworkUUID} /* ${productName} in Frameworks */`,
-  // ];
 
   return config;
 };
