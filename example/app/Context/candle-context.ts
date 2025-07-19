8acfca55bb0faa09df36c2ab99869aa9fbc71c35
Gary Tokman <yo@garytokman.com>
2025-07-19 13:00:55 -0400

    fix: add context

---
+++ example/app/Context/candle-context.ts
@@ -0,0 +1,14 @@
+import { createContext, useContext, useMemo } from "react";
+import { CandleClient } from "react-native-candle";
+
+export const CandleClientContext = createContext<CandleClient | null>(null);
+
+export function useCandleClient() {
+  const ctx = useContext(CandleClientContext);
+  if (ctx === null) {
+    throw new Error(
+      "useCandleClient must be used within a CandleClientContext provider"
+    );
+  }
+  return ctx;
+}
